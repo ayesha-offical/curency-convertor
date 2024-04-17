a89da0614a18d6dececbf93dc98880a035c57dbe
ayesha-offical <160173599+ayesha-offical@users.noreply.github.com>
2024-04-17 21:06:29 +0500

    cli currency convertor

---
+++ main.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+//for file exicution
+//SHABANG 
+import inquirer from "inquirer";
+import chalk from "chalk";
+const currency = {
+    USD: 1, //base currency
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280,
+    WON: 1383.95, //korean won
+};
+let user_Ans = await inquirer.prompt([
+    {
+        name: "fromcurrency",
+        message: chalk.blueBright("Enter From Currency: "),
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "WON"]
+    },
+    {
+        name: "Tocurrency",
+        message: chalk.yellowBright("Enter to Currency: "),
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR", "WON"]
+    },
+    {
+        name: "Amount",
+        type: "number",
+        message: chalk.blueBright("Enter Your Amount: ")
+    }
+]);
+let fromAmount = currency[user_Ans.fromcurrency]; //exchange rate
+let toAmount = currency[user_Ans.Tocurrency]; //exchange rate
+let Amount = user_Ans.Amount;
+let baseAmount = Amount / fromAmount; // usd base amount 
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
